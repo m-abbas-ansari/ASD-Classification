687cb3974900467db189c1dc90b2470faf05b84e
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2023-06-13 11:27:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,28 +14,33 @@ from tqdm import tqdm
 import wandb
 from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, f1_score
 
-from model import Sal_seq
+from model import Sal_seq, SalBert
 from data import read_dataset, ASDDataset
 
 parser = argparse.ArgumentParser(description='Autism screening based on eye-tracking data')
-parser.add_argument('--img_dir', type=str, default='../../Datasets/Saliency4ASD/TrainingData/Images', help='Directory to images')
-parser.add_argument('--anno_dir', type=str, default='../../Datasets/Saliency4ASD/TrainingData', help='Directory to annotation files')
+parser.add_argument('--img_dir', type=str, default='TrainingDataset\TrainingData\Images', help='Directory to images')
+parser.add_argument('--anno_dir', type=str, default='TrainingDataset\TrainingData', help='Directory to annotation files')
 parser.add_argument('--backend', type=str, default='resnet', help='Backend for visual encoder')
 parser.add_argument('--lr',type=float,default=1e-4,help='specify learning rate')
 parser.add_argument('--checkpoint_path',type=str,default='Checkpoints',help='Directory for saving checkpoints')
-parser.add_argument('--epoch',type=int,default=10,help='Specify maximum number of epoch')
+parser.add_argument('--epoch',type=int,default=20,help='Specify maximum number of epoch')
 parser.add_argument('--batch_size',type=int,default=12,help='Batch size')
 parser.add_argument('--max_len',type=int,default=14,help='Maximum number of fixations for an image')
-parser.add_argument('--hidden_size',type=int,default=512,help='Hidden size for RNN')
-parser.add_argument('--clip',type=float,default=10,help='Gradient clipping')
+parser.add_argument('--hidden_size',type=int,default=512,help='Hidden size for RNN/BERT')
+parser.add_argument('--clip',type=float,default=0,help='Gradient clipping')
 parser.add_argument('--img_height',type=int,default=600,help='Image Height')
 parser.add_argument('--img_width',type=int,default=800,help='Image Width')
 parser.add_argument('--t_i', type=int, default=1, help='Start index of training data')
 parser.add_argument('--t_f', type=int, default=241, help='End index of training data')
 parser.add_argument('--v_i', type=int, default=241, help='Start index of validation data')
 parser.add_argument('--v_f', type=int, default=301, help='End index of validation data')
+
+# Time Relted Arguments
 parser.add_argument('--mask', type=bool, default=False, help='Time Masking')
 parser.add_argument('--joint', type=bool, default=False, help='Joint Time Embedding')
+
+# Model Related Arguments
+parser.add_argument('--model', type=str, default='Sal_seq', help='Model to use')
 
 args = parser.parse_args()
 
@@ -46,7 +51,8 @@ transform = transforms.Compose([transforms.Resize((args.img_height,args.img_widt
 def clip_gradient(optimizer, grad_clip):
     for group in optimizer.param_groups:
         for param in group['params']:
-            param.grad.data.clamp_(-grad_clip, grad_clip)
+            if param.grad is not None :
+                param.grad.data.clamp_(-grad_clip, grad_clip)
 
 def adjust_lr(optimizer, epoch):
     "adatively adjust lr based on epoch"
@@ -112,7 +118,10 @@ def train(model, trainloader, optimizer, criterion, iteration):
     f1 = f1_score(targets, preds > 0.5)
     
     # Print metrics in one line
-    print("Train: Acc: {:.4f}, AUC: {:.4f}, Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}".format(acc, auc, precision, recall, f1))
+    print("Train: Acc: {:.4f}, AUC: {:.4f}, Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, Loss: {:.4f}".format(acc, auc, precision, recall, f1, avg_loss))
+    
+    # Log metrics on wandbs
+    wandb.log({'train acc': acc, 'train auc': auc, 'train precision': precision, 'train recall': recall, 'train f1': f1, 'train loss': avg_loss})
     
     return iteration
 
@@ -121,8 +130,9 @@ def validate(model, valloader, criterion):
     preds = []
     targets = []
     model.eval()
+    avg_loss = 0
     with torch.no_grad():
-        for img, label, fixation, duration in tqdm(valloader):
+        for i, (img, label, fixation, duration) in enumerate(tqdm(valloader)):
             if len(img) == 1: # Skip if batch size is 1
                 continue
             duration = duration.type(torch.FloatTensor)
@@ -133,7 +143,10 @@ def validate(model, valloader, criterion):
                 duration = duration.cuda()
 
             pred = model(img, fixation, duration)
-
+            
+            loss = criterion(pred, label)
+            avg_loss = (avg_loss*np.maximum(0,i) + loss.data.cpu().numpy())/(i+1)
+            
             preds.append(pred.detach().cpu().numpy())
             targets.append(label.detach().cpu().numpy())
     
@@ -148,18 +161,19 @@ def validate(model, valloader, criterion):
     f1 = f1_score(targets, preds > 0.5)
     
     # Print metrics in one line
-    print("Val: Acc: {:.4f}, AUC: {:.4f}, Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}".format(acc, auc, precision, recall, f1))
+    print("Val: Acc: {:.4f}, AUC: {:.4f}, Precision: {:.4f}, Recall: {:.4f}, F1: {:.4f}, Loss: {:.4f}".format(acc, auc, precision, recall, f1, avg_loss))
     
     # Log metrics on wandb
-    wandb.log({'val acc': acc, 'val auc': auc, 'val precision': precision, 'val recall': recall, 'val f1': f1})
+    wandb.log({'val acc': acc, 'val auc': auc, 'val precision': precision, 'val recall': recall, 'val f1': f1, 'val loss': avg_loss})
     
     return auc
         
 def main():
     print("#"*8)
     print("Wandb setup")
+    name_of_project=str(input("Name of project: "))
     name_of_run=str(input("Name of run: "))
-    wandb.init(project="asd-lstm", name=name_of_run)
+    wandb.init(project=name_of_project, name=name_of_run)
     
     # Load Data
     print("#"*8)
@@ -176,14 +190,26 @@ def main():
     val_set = ASDDataset(args.img_dir,val_anno,args.max_len,args.img_height,args.img_width,transform)
     print("Length of training set: ", len(train_set))
     print("Length of validation set: ", len(val_set))
-    trainloader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=2)
-    valloader = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+    valloader = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=True)
     
     # Model
     print("Creating model")
-    model = Sal_seq(backend=args.backend,seq_len=args.max_len,
-                    hidden_size=args.hidden_size,
-                    mask=args.mask, joint=args.joint)
+    if args.model == 'Sal_seq':
+        model = Sal_seq(backend=args.backend,seq_len=args.max_len,
+                        hidden_size=args.hidden_size,
+                        mask=args.mask, joint=args.joint)
+        print("Created Sal_seq model")
+    elif args.model == 'SalBert':
+        model = SalBert(backend=args.backend,seq_len=args.max_len)
+        print("Created SalBert model")
+    else:
+        print("Model not recognized")
+        return
+    
+    total_params = sum([p.numel() for p in model.parameters()])
+    print(f"Total parameters in model: {total_params:,}")
+        
     if args.mask or args.joint:
         print("Mask: ", args.mask)
         print("Joint: ", args.joint)
@@ -196,6 +222,7 @@ def main():
     # Optimizer
     print("Creating optimizer")
     optimizer = torch.optim.Adam(model.parameters(),lr=args.lr, weight_decay=1e-5)
+    # optimizer = torch.optim.SGD(model.parameters(),lr=args.lr)
     
     # Loss
     print("Creating loss function")
@@ -206,15 +233,22 @@ def main():
     print("Training loop")
     iteration = 0
     best_auc = 0
+    os.makedirs(f"{args.checkpoint_path}\{name_of_project}\{name_of_run}", exist_ok=True)
     for epoch in range(args.epoch):
         print("\nEpoch: ", epoch+1)
         iteration = train(model, trainloader, optimizer, criterion, iteration)
         auc = validate(model, valloader, criterion)
         if auc > best_auc:
             best_auc = auc
-            model_name = f"lstm-base-auc-{auc:.2f} " + strftime("%Y-%m-%d_%H:%M:%S", gmtime())+".pth"
-            torch.save(model.state_dict(), os.path.join(args.checkpoint_path, model_name))
+            model_name = f"{name_of_project}-{name_of_run}-bestauc-{auc:.2f}.pth"
+            model_path = os.path.join(f"{args.checkpoint_path}\{name_of_project}\{name_of_run}", model_name)
+            torch.save(model.state_dict(), model_path)
             print("Saved " + model_name)
         adjust_lr(optimizer, epoch)
-        
+
+        model_name = f"{name_of_project}-{name_of_run}-epoch-{epoch+1}.pth"
+        model_path = os.path.join(f"{args.checkpoint_path}\{name_of_project}\{name_of_run}", model_name)
+        torch.save(model.state_dict(), model_path)
+        print("Saved " + model_name)
+
 main()
